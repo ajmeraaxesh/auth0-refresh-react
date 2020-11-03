24217f4d91a3e079bf2ac100cdfb1d73d6fa6db8
Axesh R. Ajmera <ajmeraaxesh@gmail.com>
2020-11-03 15:16:18 +0530

    Basic integration of tailwindUI with Nextjs

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <div className="container mx-auto">
+            <h1 className=" text-4xl text-indigo-500">Hello, NextJs</h1>
+        </div>
+    )
+}
