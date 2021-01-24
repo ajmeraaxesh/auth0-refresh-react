f973e548361fc2fba42e58205666f5d0280c5ae9
Axesh R Ajmera <ajmeraaxesh@gmail.com>
2021-01-24 10:51:19 +0530

    Update pagehead.js

--- src/components/pagehead.js
+++ src/components/pagehead.js
@@ -7,15 +7,9 @@ const PageHead = ({ children, title }) => {
             {/**TODO:FIXME: Remove the fonts if custom fonts are needed. Also uncomment in tailwind.config.js and globals.css */}
             {/* <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" /> */}
             <Head>
-                <meta charSet="UTF-8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
-                <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-
+                <meta name="viewport" content="width=device-width"/>
+                <meta charset="utf-8" />
                 <title>{title}</title>
-
                 {children}
             </Head>
         </>
