90650f6702189b0af9b205d4ed1d0c73446350c7
Axesh R. Ajmera <ajmeraaxesh@gmail.com>
2021-05-26 07:03:14 +0530

    Minor fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 export default function Home() {
     const router = useRouter()
-    router.push('/dashboard')
+    useEffect(() => {
+        router.push('/dashboard')
+    }, [])
+
     return null
 }
